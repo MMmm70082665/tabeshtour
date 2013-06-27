66be81430d557ebc335b2b8764e0741ef51f4b27
MMmm70082665 <git.account517@yahoo.com>
2013-06-26 17:47:38 -0700

    4

---
+++ app/modules/Admin/views/Post/EditInputView.class.php
@@ -0,0 +1,13 @@
+<?php
+
+class Admin_Post_EditInputView extends agentAdminBaseView
+{
+	public function executeHtml(AgaviRequestDataHolder $rd)
+	{
+		$this->setupHtml($rd);
+		
+		$this->setAttribute('_title', 'Post.Edit');
+	}
+}
+
+?>
